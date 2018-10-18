9375d62a4bc75f02e3a1f116134779c4e5a9e8a1
Teddy Ngwenya <tngwenya@mhg.co.za>
2018-10-18 23:53:07 +0200

    add jpa entinties

--- spring5webapp/src/main/java/org/unndevs/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/org/unndevs/spring5webapp/model/Author.java
@@ -1,3 +1,24 @@
 package org.unndevs.spring5webapp.model;
 
-public class Author {}
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class Author {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String isbn;
+  private String title;
+  private String publisher;
+  @ManyToMany(mappedBy = "authors")
+  private Set<Book> books = new HashSet<>();
+}
--- spring5webapp/src/main/java/org/unndevs/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/org/unndevs/spring5webapp/model/Book.java
@@ -1,3 +1,26 @@
 package org.unndevs.spring5webapp.model;
 
-public class Book {}
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class Book {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String name;
+  private String surname;
+  @ManyToMany
+  @JoinTable(name ="author_book", joinColumns = @JoinColumn(name = "author_id"), inverseJoinColumns = @JoinColumn(name = "book_id"))
+  private Set<Author> authors = new HashSet<>();
+}
